af6f84fc96fb7a186168430e6d11fbc5d9505672
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-04-06 20:48:17 +0000

    final

--- task_01.py
+++ task_01.py
@@ -4,21 +4,21 @@
 
 GRADE_DATA = {
     'Luke Skywalker': {
-        'math':'B',
-        'etiquette':'B+',
-        'grammar':'B',
-        'gym':'A'
+        'math': 'B',
+        'etiquette': 'B+',
+        'grammar': 'B',
+        'gym': 'A'
         },
     'Han Solo': {
-        'math':'A-',
-        'etiquette':'C-',
-        'grammar':'B',
-        'gym':'B' 
+        'math': 'A-',
+        'etiquette': 'C-',
+        'grammar': 'B',
+        'gym': 'B'
     },
     'C-3PO': {
-        'math':'C', 
-        'etiquette':'A+', 
-        'grammar':'A', 
-        'gym':'F'
+        'math': 'C',
+        'etiquette': 'A+',
+        'grammar': 'A',
+        'gym': 'F'
     },
 }
